f7dd8c7e8c10c9f8062ca54f6574f72e189ac3e4
sauderx <ronaldpradopalacios@gmail.com>
2026-06-27 18:01:52 -0400

    Se agrega selector de tallas y cantidad

--- app/(shop)/category/[id]/page.tsx
+++ app/(shop)/category/[id]/page.tsx
@@ -1,21 +1,44 @@
+import { ProductGrid, Title } from "@/components";
 import { notFound } from "next/navigation";
+import { initialData } from "@/seed/seed";
+import { Category } from "@/interfaces/product.interface";
 
 interface Props {
   params: Promise<{
-    id: string;
+    id: Category;
   }>;
 }
 
+const seedProducts = initialData.products;
+
 export default async function ({ params }: Props) {
   const { id } = await params;
+  const categoriesIds: Category[] = ["men", "women", "kid"];
 
-  if (id === "kids") {
+  if (!categoriesIds.includes(id)) {
     notFound();
   }
+  // if (id === "kids") {
+  //   notFound();
+  // }
+
+  const products = seedProducts.filter((product) => product.gender === id);
+
+  const labels: Record<Category, string> = {
+    men: "hombres",
+    women: "mujeres",
+    kid: "niños",
+    unisex: "todos",
+  };
+
+  const title = `Artículos para ${labels[id]}`;
+  const subtitle = `Productos para ${labels[id]}`;
 
   return (
-    <div>
-      <h1>Category Page {id}</h1>
-    </div>
+    <>
+      <Title title={title} subtitle={subtitle} className="mb-2" />
+
+      <ProductGrid products={products} />
+    </>
   );
 }
--- app/(shop)/product/[slug]/page.tsx
+++ app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,51 @@
-export default function (){
+import { QuantitySelector } from "@/components";
+import { SizeSelector } from "@/components/product/size-selector/SizeSelector";
+import { titleFont } from "@/config/fonts";
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: Promise<{
+    slug: string;
+  }>;
+}
+
+export default async function ({ params }: Props) {
+  const { slug } = await params;
+  const product = initialData.products.find((product) => product.slug === slug);
+
+  if (!product) {
+    notFound();
+  }
+
   return (
-    <div>
-      <h1>Product Page</h1>
+    <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+      {/* Slide show */}
+      <div className="col-span-1 md:col-span-2"> hola</div>
+
+      {/* Detalles del producto */}
+      <div className="col-span-1 px-5">
+        <h1 className={`${titleFont.className} antialiased font-bol text-xl`}>
+          {product.title}
+        </h1>
+        <p className="text-lg mb-5">${product.price}</p>
+
+        {/* Selector de tallas */}
+        <SizeSelector
+          selectedSize={product.sizes[0]}
+          availableSizes={product.sizes}
+        />
+
+        {/* Selector de cantidad  */}
+        <QuantitySelector quantity={1} />
+
+        {/* Boton */}
+        <button className="btn-primary my-5">Agregar al carrito</button>
+
+        {/* Descripción */}
+        <h3 className="font-bold text-sm">Descripción</h3>
+        <p className="font-light">{product.description}</p>
+      </div>
     </div>
   );
 }
